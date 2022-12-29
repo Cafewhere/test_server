3cd19fa7d8f509ff0ab6571d7ba9a048108748cd
eunbi lee <led156@naver.com>
2022-12-29 14:12:43 +0900

    [add] test func

--- test.py
+++ test.py
@@ -17,5 +17,21 @@ def get_test():
 def print_res():
     return returnJson.getTest()
 
+@app.route('/api/test', methods=['POST'])
+def print_msg():
+    foo = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "간단한 텍스트 요소입니다."
+                    }
+                }
+            ]
+        }
+    }
+    return json.dumps(foo)
+
 if __name__ == "__main__":
     app.run()
